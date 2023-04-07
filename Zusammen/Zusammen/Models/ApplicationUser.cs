881582024b441569a0e1cc728dda20fa59a887ea
Carmen Thom <thomrya22@outlook.com>
2023-04-07 17:04:08 -0500

    Created ApplicationUser Boiler

---
+++ Zusammen/Zusammen/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Zusammen.Models
+{
+	public class ApplicationUser : IdentityUser
+    {
+		public ApplicationUser()
+		{
+		}
+	}
+}
+
