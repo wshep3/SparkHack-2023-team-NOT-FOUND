26685cd8b9956ac58bae8d6db15ab198aec5b3cf
Carmen Thom <thomrya22@outlook.com>
2023-04-07 20:30:43 -0500

    Display names and styles for register

--- Zusammen/Zusammen/Models/ApplicationUser.cs
+++ Zusammen/Zusammen/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Zusammen.Models
@@ -9,20 +10,31 @@ namespace Zusammen.Models
         Sophemore,
         Junior,
         Senior,
+        [Display(Name = "Super Senior")]
         superSenior,
+        [Display(Name = "Graduate Student")]
         gradStudent
     }
 
     public enum SchoolHousing
     {
+        [Display(Name = "Academic and Residential Complex")]
         arc,
+        [Display(Name = "Commons North")]
         cn,
+        [Display(Name = "Commons West")]
         cw,
+        [Display(Name = "Courtyard")]
         cty,
+        [Display(Name = "James Stukel Towers")]
         jst,
+        [Display(Name = "Marie Robinson Hall")]
         mrh,
+        [Display(Name = "Single Student Residence")]
         ssr,
+        [Display(Name = "Thomas Bekham Hall")]
         tbh,
+        [Display(Name = "Commuter")]
         commuter
     }
     public class ApplicationUser : IdentityUser
