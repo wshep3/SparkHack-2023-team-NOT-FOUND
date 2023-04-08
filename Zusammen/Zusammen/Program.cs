82448c478ffa164583c9657a023ea5a3b7befdbf
Carmen Thom <thomrya22@outlook.com>
2023-04-08 09:26:53 -0500

    removed required confirment account

--- Zusammen/Zusammen/Program.cs
+++ Zusammen/Zusammen/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
