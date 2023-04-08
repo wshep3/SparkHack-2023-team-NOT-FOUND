0c5d5e41d53e43f2a7655114349d35c9e973e687
Chris Wood <118241499+Cwood00@users.noreply.github.com>
2023-04-07 23:12:04 -0500

    Added Major enum type

--- Zusammen/Zusammen/Models/ApplicationUser.cs
+++ Zusammen/Zusammen/Models/ApplicationUser.cs
@@ -37,6 +37,156 @@ namespace Zusammen.Models
         [Display(Name = "Commuter")]
         commuter
     }
+
+    public enum Major
+    {
+        Accounting,
+        Acting,
+        Anthropology,
+        [Display(Name = "Applied Psychology")]
+        AppliedPsychology,
+        [Display(Name = "Architectural Studies")]
+        ArchitecturalStudies,
+        Architecure,
+        Art,
+        [Display(Name = "Art History")]
+        ArtHistory,
+        Biochemistry,
+        [Display(Name = "Biological Sciences")]
+        BiologicalSciences,
+        [Display(Name = "Biomedical Engineering")]
+        BiomedicalEngineering,
+        [Display(Name = "Biopharmaceutical Sciences")]
+        BiopharmaceuticalSciences,
+        [Display(Name = "Black Studies")]
+        BlackStudies,
+        [Display(Name = "Chemical Engineering")]
+        ChemicalEngineering,
+        Chemistry,
+        [Display(Name = "Civil Engineering")]
+        CivilEngineering,
+        [Display(Name = "Classical Studies")]
+        ClassicalStudies,
+        Communications,
+        [Display(Name = "Computer Engineering")]
+        ComputerEngineering,
+        [Display(Name = "Computer Science")]
+        ComputerScience,
+        [Display(Name = "Computer Science and Design")]
+        ComputerScienceAndDesign,
+        [Display(Name = "Computer Science and Linguistics")]
+        ComputerScienceAndLinguistics,
+        [Display(Name = "Criminology Law and Justice")]
+        CriminologyLawAndJustice,
+        [Display(Name = "Desgin Studies")]
+        DesginStudies,
+        [Display(Name = "Disability and Human Development")]
+        DisabilityAndHumanDevelopment,
+        [Display(Name = "Earth and Enviromental Sciences")]
+        EarthAndEnviromentalSciences,
+        Economics,
+        [Display(Name = "Electriacl Engineering")]
+        ElectriaclEngineering,
+        [Display(Name = "Engineering Managment")]
+        EngineeringManagment,
+        [Display(Name = "Engineering Physics")]
+        EngineeringPhysics,
+        English,
+        Entrepreneurship,
+        [Display(Name = "Enviornmental Engineering")]
+        EnviornmentalEngineering,
+        Finance,
+        [Display(Name = "French and Francophone Studies")]
+        FrenchAndFrancophoneStudies,
+        [Display(Name = "Gender and Women's Studies")]
+        GenderAndWomensStudies,
+        [Display(Name = "Germanic Studies")]
+        GermanicStudies,
+        [Display(Name = "Global Asian Studies")]
+        GlobalAsianStudies,
+        [Display(Name = "Graphic Design")]
+        GraphicDesign,
+        [Display(Name = "Health Inormation Management")]
+        HealthInormationManagement,
+        History,
+        [Display(Name = "Human Development and Learning")]
+        HumanDevelopmentAndLearning,
+        [Display(Name = "Human Resource Managment")]
+        HumanResourceManagment,
+        [Display(Name = "Industrial Design")]
+        IndustrialDesign,
+        [Display(Name = "Industrial Engineering")]
+        IndustrialEngineering,
+        [Display(Name = "Information and Decision Sciences")]
+        InformationAndDecisionSciences,
+        [Display(Name = "Integrated Health Studies")]
+        IntegratedHealthStudies,
+        [Display(Name = "Interdisciplinary Education in the Arts")]
+        InterdisciplinaryEducationInTheArts,
+        Italian,
+        [Display(Name = "Jazz Studies")]
+        JazzStudies,
+        Kinesiology,
+        [Display(Name = "Latin American and Latino Studies")]
+        LatinAmericanAndLatinoStudies,
+        [Display(Name = "Liberal Studies")]
+        LiberalStudies,
+        Management,
+        Marketing,
+        Mathematics,
+        [Display(Name = "Mathematics and Computer Science")]
+        MathematicsAndComputerScience,
+        [Display(Name = "Mechanical Engineering")]
+        MechanicalEngineering,
+        Music,
+        [Display(Name = "Music Business")]
+        MusicBusiness,
+        Neuroscience,
+        Nursing,
+        Nutrition,
+        Performance,
+        Pharmacy,
+        Philosophy,
+        Physics,
+        [Display(Name = "Polish Russian and Central and Eastern European Studies")]
+        PolishRussianAndCentralAndEasternEuropeanStudies,
+        [Display(Name = "Political Science")]
+        PoliticalScience,
+        Psychology,
+        [Display(Name = "Public Health")]
+        PublicHealth,
+        [Display(Name = "Public Policy")]
+        PublicPolicy,
+        [Display(Name = "Real Estate")]
+        RealEstate,
+        [Display(Name = "Rehabilitation Sciences")]
+        RehabilitationSciences,
+        Sociology,
+        Spanish,
+        Statistics,
+        [Display(Name = "Teaching of English")]
+        TeachingOfEnglish,
+        [Display(Name = "Teaching of French")]
+        TeachingOfFrench,
+        [Display(Name = "Teaching of German")]
+        TeachingOfGerman,
+        [Display(Name = "Teaching of History")]
+        TeachingOfHistory,
+        [Display(Name = "Teaching of Mathematics")]
+        TeachingOfMathematics,
+        [Display(Name = "Teaching of Physics")]
+        TeachingOfPhysics,
+        [Display(Name = "Teaching of Spanish")]
+        TeachingOfSpanish,
+        [Display(Name = "Theatre and Performance")]
+        TheatreAndPerformance,
+        [Display(Name = "Urban Education")]
+        UrbanEducation,
+        [Display(Name = "Urban Studies")]
+        UrbanStudies
+
+    }
+
     public class ApplicationUser : IdentityUser
     {
         public ApplicationUser() { }
