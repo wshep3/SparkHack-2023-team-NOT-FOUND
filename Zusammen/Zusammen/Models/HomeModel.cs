4971180a23ac40b45f7868c0312534a81c3179be
Carmen Thom <thomrya22@outlook.com>
2023-04-08 14:37:47 -0500

    seiruhesr

---
+++ Zusammen/Zusammen/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace Zusammen.Models
+{
+	public class HomeModel
+	{
+
+		public List<int> items { get; set; }
+		public List<ApplicationUser> appUsers { get; set; }
+	}
+}
+
