a37decbe7ec7796f6673448708f984d9c83aa79a
wshep3 <126131553+wshep3@users.noreply.github.com>
2023-04-08 10:47:24 -0500

    Update User profile

--- Zusammen/Zusammen/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Zusammen/Zusammen/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -7,8 +7,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zusammen.Data;
 using Zusammen.Models;
 
 namespace Zusammen.Areas.Identity.Pages.Account.Manage
@@ -52,25 +55,27 @@ namespace Zusammen.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public class InputModel
         {
+            [Display]
+            public String name { get; set; }
+
+            [Display]
+            public Gender gender { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                
             };
         }
 
@@ -89,6 +94,8 @@ namespace Zusammen.Areas.Identity.Pages.Account.Manage
         public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+            var store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -96,20 +103,17 @@ namespace Zusammen.Areas.Identity.Pages.Account.Manage
 
             if (!ModelState.IsValid)
             {
+
                 await LoadAsync(user);
+
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
+
+            user.gender = Input.gender;
+            user.name = Input.name;
+
+            var result = _userManager.UpdateAsync(user);
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
--- Zusammen/Zusammen/Data/ApplicationDbContext.cs
+++ Zusammen/Zusammen/Data/ApplicationDbContext.cs
@@ -6,6 +6,10 @@ namespace Zusammen.Data;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public ApplicationDbContext()
+    {
+    }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
