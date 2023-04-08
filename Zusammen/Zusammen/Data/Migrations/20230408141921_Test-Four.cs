84f21262993f321a1f39822da66a8f595951669f
wshep3 <126131553+wshep3@users.noreply.github.com>
2023-04-08 09:22:13 -0500

    New migration

---
+++ Zusammen/Zusammen/Data/Migrations/20230408141921_Test-Four.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zusammen.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestFour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "gender",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "AspNetUsers");
+        }
+    }
+}
